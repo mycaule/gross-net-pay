f703ba76350e156aa08755846ac950022a66910f
Michel Hua <mycaule@users.noreply.github.com>
2014-12-22 07:35:56 +0100

    Travis config update

--- spec/protractor-conf.js
+++ spec/protractor-conf.js
@@ -1,5 +1,8 @@
 exports.config = {
   // seleniumAddress: 'http://localhost:4444/wd/hub',
   directConnect: true,
-  specs: ['spec.js']
+  specs: ['spec.js'],
+  capabilities: {
+    'browserName': 'firefox'
+  }
 };
